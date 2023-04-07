8eb0ef10b7b5b5c22af5abcc2ddaac46aa9d2228
Matt Wallace <mattwallace140@gmail.comgit config --global user.email mattwallace140@gmail.com>
2023-04-06 20:16:33 -0400

    all tasks complete

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ class Car {
 
 class Lambdasian {
   constructor({name,age,location}){
-    this.name =name;
+    this.name = name;
     this.age = age;
     this.location = location;
   }
@@ -146,8 +146,19 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 
-class Instructor {
-
+class Instructor extends Lambdasian{
+  constructor({name,age,location,specialty,favLanguage,catchPhrase}){
+    super({name,age,location,specialty,favLanguage,catchPhrase})
+    this.specialty = specialty;
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase;
+}
+demo(subject){
+  return `Today we are learning about ${subject}`;
+}
+grade(student,subject){
+  return `${student.name} receives a perfect score on ${subject}`;
+}
 }
 
 /*
@@ -166,8 +177,22 @@ class Instructor {
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
 
-class Student {
-   
+class Student extends Lambdasian{
+   constructor({name,age,location,previousBackground, className, favSubjects}){
+    super({name,age,location,previousBackground, className, favSubjects});
+    this.previousBackground = previousBackground;
+    this.className = className;
+    this.favSubjects = favSubjects;
+   }
+   listSubjects(){
+    return `Loving ${this.favSubjects}`
+   }
+   PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`
+   }
+   sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`
+   }
 }
 
 /*
@@ -184,8 +209,18 @@ class Student {
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
-class ProjectManager {
-   
+class ProjectManager extends Instructor{
+   constructor({name,age,location,specialty,favLanguage,catchPhrase,gradClassName,favInstructor}){
+    super({name,age,location,specialty,favLanguage,catchPhrase,gradClassName,favInstructor,});
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+   }
+   standUp(channel){
+    return `${this.name} announces to ${channel}, @channel standy times!`
+   }
+   debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`
+   }
 }
 
 /*
